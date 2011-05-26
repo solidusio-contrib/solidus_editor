023da8c132395e1ef9bc3655ca1faa624c5dbaf0
Roman Smirnov <roman@railsdog.com>
2011-05-26 15:54:19 +0400

    Switch to Deface

--- lib/spree_editor_hooks.rb
+++ lib/spree_editor_hooks.rb
@@ -1,18 +1,24 @@
-class SpreeEditorHooks < Spree::ThemeSupport::HookListener
-  # Admin configuration
-  insert_after :admin_configurations_menu do
-    %(<tr>
-      <td><%= link_to t("rich_editor"), admin_editor_settings_path %></td>
-      <td><%= t("rich_editor_description") %></td>
-      </ tr>)
-  end
+Deface::Override.new(
+  :virtual_path => "admin/configurations/index",
+  :name => "spree_editor_admin_configurations_menu",
+  :insert_bottom => "[data-hook='admin_configurations_menu']",
+  :text => %(
+  <tr>
+    <td><%= link_to t("rich_editor"), admin_editor_settings_path %></td>
+    <td><%= t("rich_editor_description") %></td>
+  </tr>),
+  :disabled => false)
 
-  insert_after :admin_configurations_sidebar_menu do
-    %(<li<%== ' class="active"' if controller.controller_name == 'editor_settings' %>><%= link_to t("rich_editor"), admin_editor_settings_path %></li>)
-  end
+Deface::Override.new(
+  :virtual_path => "admin/shared/_configuration_menu",
+  :name => "spree_editor_admin_configurations_sidebar_menu",
+  :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+  :text => %(<%= content_tag(:li, :class => controller.controller_name == 'editor_settings' ? 'active' : nil) { link_to t('rich_editor'), admin_editor_settings_path } %>),
+  :disabled => false)
 
-  # Javascript
-  insert_after :admin_inside_head do
-    %(<%= render :partial => 'shared/rich_editor_javascript' %>)
-  end
-end
+Deface::Override.new(
+  :virtual_path => "layouts/admin",
+  :name => "spree_editor_admin_inside_head",
+  :insert_bottom => "[data-hook='admin_footer_scripts']",
+  :partial => 'shared/rich_editor_javascript',
+  :disabled => false)

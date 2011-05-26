fd19043e9d4c79cd279d07f66323570664f0a93d
Roman Smirnov <roman@railsdog.com>
2011-05-26 15:52:08 +0400

    Updated work with settings

--- app/controllers/admin/editor_settings_controller.rb
+++ app/controllers/admin/editor_settings_controller.rb
@@ -1,14 +1,8 @@
 class Admin::EditorSettingsController < Admin::BaseController
   def update
-    preferences = params[:preferences] || {}
-    enable_editor = preferences[:enabled].nil? ? false : true;
-    preferences.update(:enabled => enable_editor)
-    Spree::Editor::Config.set(preferences)
-
-    respond_to do |format|
-      format.html {
-        redirect_to admin_editor_settings_path
-      }
-    end
+    @config = Spree::Editor::Config.instance
+    @config.update_attributes(params[@config.class.name.underscore])
+    Rails.cache.delete("configuration_#{@config.class.name}".to_sym)
+    redirect_to admin_editor_settings_path
   end
 end
--- lib/editor_configuration.rb
+++ lib/editor_configuration.rb
@@ -1,5 +1,5 @@
 class EditorConfiguration < Configuration
   preference :enabled, :boolean, :default => true
   preference :current_editor, :string, :default => 'YUIRichEditor'
-  preference :ids, :string, :default => 'product_description page_body'
+  preference :ids, :text, :default => 'product_description page_body'
 end

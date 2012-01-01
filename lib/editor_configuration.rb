a6547a4b1bb5919ff5b3888820652f9bfb305da5
Brice Sanchez <contact@brice-sanchez.com>
2012-01-01 16:26:18 -0500

    Switch default editor ofr WYMEditor

--- lib/editor_configuration.rb
+++ lib/editor_configuration.rb
@@ -1,5 +1,5 @@
 class EditorConfiguration < Configuration
   preference :enabled, :boolean, :default => true
-  preference :current_editor, :string, :default => 'TinyMCE'
+  preference :current_editor, :string, :default => 'WYMEditor'
   preference :ids, :text, :default => 'product_description page_body'
 end

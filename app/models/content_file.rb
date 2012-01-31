d43e9b0df7faad60fc0426f5afd8018e818a378c
Alexander Balashov <divineforest@gmail.com>
2012-01-31 14:23:33 +0400

    'Spree' namespace for Asset and Image models

--- app/models/content_file.rb
+++ app/models/content_file.rb
@@ -1,4 +1,4 @@
-class ContentFile < Asset
+class ContentFile < Spree::Asset
   has_attached_file :attachment,
     :url => "/assets/content_files/:id/:basename.:extension",
     :path => ":rails_root/public/assets/content_files/:id/:basename.:extension"

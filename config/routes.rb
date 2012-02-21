f212f77049c80dd608796bca889459c505d5670b
Peter Berkenbosch <peter@pero-ict.nl>
2012-02-21 16:50:45 +0100

    remove the routes for custom images and files (for now)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Spree::Core::Engine.routes.draw do
   namespace :admin do
     resource :editor_settings, :only => ['show', 'update', 'edit']
   end
-
-  resources :content_images, :only => [:create]
-  resources :content_files, :only => [:create]
 end

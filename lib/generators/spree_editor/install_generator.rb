c65b352bfef391f3c3d689a00f32b27cb32f9628
Marc Lee <marc@adviso.com.au>
2012-06-15 14:33:17 +1000

    create a template tinymce config file so we could change it in there instead. installs it on the system.

--- lib/generators/spree_editor/install_generator.rb
+++ lib/generators/spree_editor/install_generator.rb
@@ -1,6 +1,7 @@
 module SpreeEditor
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../../templates", __FILE__)
 
       def add_javascripts
         append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_editor\n"
@@ -10,6 +11,9 @@ module SpreeEditor
         inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_editor\n", :before => /\*\//, :verbose => true
       end
 
+      def copy_tinymce_file
+        copy_file "config/tinymce.yml", "config/tinymce.yml"
+      end
     end
   end
 end

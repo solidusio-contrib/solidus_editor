db3ebebb8d66214873c1a5ec776fc072a7ea8041
Randy Terbush <randy@terbush.org>
2013-05-09 09:40:57 -0600

    Port to Spree 2.0 translations

--- app/overrides/add_rich_editor_link.rb
+++ app/overrides/add_rich_editor_link.rb
@@ -2,6 +2,6 @@ Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
                      :name => "add_rich_editor_link",
                      :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
                      :text => "<tr>
-      <td><%= link_to t(\"rich_editor\"), admin_editor_settings_path %></td>
-      <td><%= t(\"rich_editor_description\") %></td>
+      <td><%= link_to Spree.t(\"rich_editor\"), admin_editor_settings_path %></td>
+      <td><%= Spree.t(\"rich_editor_description\") %></td>
       </tr>")
--- app/overrides/add_rich_editor_tab.rb
+++ app/overrides/add_rich_editor_tab.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                      :name => "add_rich_editor_tab",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-                     :text => "<li<%== ' class=\"active\"' if controller.controller_name == 'editor_settings' %>><%= link_to t(\"rich_editor\"), admin_editor_settings_path %></li>")
+                     :text => "<li<%== ' class=\"active\"' if controller.controller_name == 'editor_settings' %>><%= link_to Spree.t(\"rich_editor\"), admin_editor_settings_path %></li>")

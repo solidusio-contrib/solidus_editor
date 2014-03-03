2adfa821a04cf1ee7469d5fe3a2b52abd61cc3f2
Jeff Dutil <jdutil@burlingtonwebapps.com>
2014-03-03 20:44:38 +0000

    Config settings dont exist anymore.

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,8 +1,8 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   has_attached_file :data
 
-  Ckeditor::AttachmentFile.attachment_definitions[:data][:path] = Spree::Config[:attachment_path]
-  Ckeditor::AttachmentFile.attachment_definitions[:data][:url] = Spree::Config[:attachment_url]
+  Ckeditor::AttachmentFile.attachment_definitions[:data][:path] = '/:class/:id/:style/:basename.:extension'
+  Ckeditor::AttachmentFile.attachment_definitions[:data][:url] = '/:class/:id/:style/:basename.:extension'
 
   validates_attachment_size :data, :less_than => 100.megabytes
   validates_attachment_presence :data
--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,8 +1,8 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data, :styles => { :content => '800>', :thumb => '118x100#' }
 
-  Ckeditor::Picture.attachment_definitions[:data][:path] = Spree::Config[:attachment_path]
-  Ckeditor::Picture.attachment_definitions[:data][:url] = Spree::Config[:attachment_url]
+  Ckeditor::Picture.attachment_definitions[:data][:path] = '/:class/:id/:style/:basename.:extension'
+  Ckeditor::Picture.attachment_definitions[:data][:url] = '/:class/:id/:style/:basename.:extension'
 
   validates_attachment_size :data, :less_than => 2.megabytes
   validates_attachment_presence :data

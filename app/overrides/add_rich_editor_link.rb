7728db154fbb71915bb81ef8eb211d961f2a3382
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-09-16 11:52:10 -0400

    Remove unused override. Fixes #79

--- app/overrides/add_rich_editor_link.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
-                     :name => "add_rich_editor_link",
-                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
-                     :text => "<tr>
-      <td><%= link_to Spree.t(:rich_editor), admin_editor_settings_path %></td>
-      <td><%= Spree.t(:rich_editor_description) %></td>
-      </tr>")

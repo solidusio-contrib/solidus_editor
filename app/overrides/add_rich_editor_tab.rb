0ac3054163f5d61387bfdfb51ed6fddcfb3ca2bb
Tobias Bohwalli <hi@futhr.io>
2015-03-11 19:41:46 +0100

    Fixes configuration path in override.

--- app/overrides/add_rich_editor_tab.rb
+++ app/overrides/add_rich_editor_tab.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/sub_menu/_configuration',
   name: 'add_rich_editor_tab',
   insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-  text: '<li<%== " class=\"active\"" if controller.controller_name == "editor_settings" %>><%= link_to Spree.t(:rich_editor), admin_editor_settings_path %></li>'
+  text: '<li<%== " class=\"active\"" if controller.controller_name == "editor_settings" %>><%= link_to Spree.t(:rich_editor), edit_admin_editor_settings_path %></li>'
 )

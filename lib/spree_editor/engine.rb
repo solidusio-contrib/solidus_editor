26c9a194ab6484fcbcce1c0ea756b34effc4e553
Alessandro Lepore <lepore.ale@gmail.com>
2015-11-21 09:29:36 +0100

    Deface: don't manually require override files
    as this can cause problems for precompiling

--- lib/spree_editor/engine.rb
+++ lib/spree_editor/engine.rb
@@ -14,7 +14,7 @@ module SpreeEditor
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
-      cache_klasses = %W(#{config.root}/app/**/*_decorator*.rb #{config.root}/app/overrides/*.rb)
+      cache_klasses = %W(#{config.root}/app/**/*_decorator*.rb)
       Dir.glob(cache_klasses) do |klass|
         Rails.configuration.cache_classes ? require(klass) : load(klass)
       end
